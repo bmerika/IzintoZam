b04df06c5a7542bd238fb9a4258059a82142c572
bmerika <bmerika02@gmail.com>
2017-10-07 14:47:16 +0200

    Update servers.js

--- servers.js
+++ servers.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 const port = process.env.PORT || 3000
-app.listen(3000, function () {
-  console.log('Example app listening on port' + port)
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port)
 })
